05d64de9a66f611c8647b093a4e330fa3ac0433c
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-10-13 11:57:38 -0400

    Add UUID.init?(lowercaseHexString:)
    Though it turns out 10C GUIDs are 40 characters, not 32, so an extra
    octet more than a standard UUID. Hrm.

--- src/UUID+StringEncoding.swift
+++ src/UUID+StringEncoding.swift
@@ -5,4 +5,31 @@ extension StringEncoding {
     var lowercaseHexString: String {
         return uuidString.lowercased().components(separatedBy: "-").joined()
     }
+
+    init?(lowercaseHexString: String) {
+        // E621E1F8-C36C-495A-93FC-0C247A3E6E5F
+        // 4 - 2 - 2 - 2 - 6
+        let uppercase = lowercaseHexString.uppercased()
+        guard uppercase.components(separatedBy: CharacterSet(charactersIn: "0123456789ABCDEF").inverted).count == 1 else {
+            preconditionFailure("\(#function): ERROR: non-hex characters in: \(lowercaseHexString)")
+        }
+
+        let utf8 = uppercase.utf8
+        guard utf8.count == 32 else {
+            preconditionFailure("\(#function): ERROR: expected 32 characters corresponding to 16 bytes, "
+                + "found \(utf8.count) instead in: \(lowercaseHexString)")
+        }
+
+        var rest = utf8
+        let chunks: [String] = [4, 2, 2, 2, 6].map { (byteCount: Int) -> String in
+            let characterCount = byteCount * 2
+            let chunk: String.UTF8View = utf8.prefix(characterCount)
+            rest = rest.dropFirst(characterCount)
+            guard let string = String(chunk) else {
+                preconditionFailure("\(#function): ERROR: failed to stringify chunk: \(chunk)")
+            }
+            return string
+        }
+        self.init(uuidString: chunks.joined(separator: "-"))
+    }
 }
