cba1df5bf122742306f5afd3cf0c73d16f3b6618
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2017-01-16 21:39:01 -0500

    Percent encode +, not space, for forms
    [fixes #29]

--- src/URLRequest+FormEncoding.swift
+++ src/URLRequest+FormEncoding.swift
@@ -4,11 +4,21 @@ private typealias FormEncoding = URLRequest
 extension FormEncoding {
     mutating func attachURLEncodedFormData(_ queryItems: [URLQueryItem]) {
         setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+
+        guard !queryItems.isEmpty else { return }
+
         // If we have to do this ourselves, or we get into multipart soonish, see:
         // https://www.w3.org/TR/html401/interact/forms.html#h-17.13.4
         var components = URLComponents()
         components.queryItems = queryItems
-        let query = components.percentEncodedQuery
-        httpBody = query?.data(using: .utf8)
+        guard let urlEncodedQuery = components.percentEncodedQuery else {
+            print("ENCODING: WARNING: Empty percent-encoded query created from", queryItems.count, "query items")
+            return
+        }
+
+        let formURLEncodedQuery = urlEncodedQuery
+            .replacingOccurrences(of: "+", with: "%2B")
+            .replacingOccurrences(of: "%20", with: "+")
+        httpBody = formURLEncodedQuery.data(using: .utf8)
     }
 }
