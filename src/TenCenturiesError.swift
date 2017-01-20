a4bafb4c8c05d75e63c6e20835e0882a3c660b33
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2017-01-19 23:01:54 -0500

    Improve doc comments
    [#32]

--- src/TenCenturiesError.swift
+++ src/TenCenturiesError.swift
@@ -3,9 +3,19 @@ import Foundation
 enum TenCenturiesError: Error {
     case notHTTP(url: URL)
     case badResponse(url: URL, data: Data?, comment: String)
+
     case parseError(url: URL, object: Any, comment: String)
     case missingField(field: String, object: JSONDictionary)
     case badFieldType(field: String, expected: Any, found: Any, in: JSONDictionary)
-    /// The backend sent us a `{ meta: { code: Int, text: String }, data: false }` response.
+
+    /**
+     The backend sent us a response like `{ meta: { code: Int, text: String }, data: false }`.
+
+     - `code` is dev info.
+     - `text` is user-presentable.
+     - `comment` is info added by our infra code providing details on the request and to what URL it went wrong.
+
+     - NOTE: This should always be shown to the user!
+     */
     case api(code: Int, text: String, comment: String)
 }
