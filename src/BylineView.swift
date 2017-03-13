bde5c5fe4f579d276411aa398558264351d1a818
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2017-03-13 14:51:23 -0400

    Use (unconfigured) AvatarImageView in BylineView
    [#25]

--- src/BylineView.swift
+++ src/BylineView.swift
@@ -11,12 +11,13 @@ class BylineView: UIView {
     }
 
     func configure(imageURL: URL, author: String, date: Date) {
+        // (jeremy-w/2017-03-13)FIXME: We need to plumb through the full account to do the avatar image view right!
         avatar.kf.setImage(with: imageURL)
         self.author.text = author
         self.date.text = PostCell.dateFormatter.string(from: date)
     }
 
-    @IBOutlet var avatar: UIImageView!
+    @IBOutlet var avatar: AvatarImageView!
     @IBOutlet var author: UILabel!
     @IBOutlet var date: UILabel!
 }
