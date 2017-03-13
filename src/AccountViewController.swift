c8d224dff624c3b32f35bf2cfa16d0591e980655
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2017-03-13 14:54:01 -0400

    Add empty AccountViewController
    [#25]

---
+++ src/AccountViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class AccountViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
