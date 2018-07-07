f05646c86d34b92ef2d991b48efe5277240666bd
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2018-07-07 13:33:13 -0400

    Delete obsolete NSRange.description override
    Verified that NSRange now formats itself as `{location, length}`,
    so I'm not losing anything.

--- src/NSRange+CustomStringConvertible.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension NSRange: CustomStringConvertible {
-    // The default is just something like _C.NSRange. Useless!
-    public var description: String {
-        return "NSRange(\(self.location), \(self.length))"
-    }
-}
